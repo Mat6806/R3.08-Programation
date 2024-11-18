128fa87735de1d155d1c09be2bdb1377e930b1dd
Mat6806 <147594936+Mat6806@users.noreply.github.com>
2024-11-18 15:06:03 +0000

    V1.2

--- Client.py
+++ Client.py
@@ -1,19 +1,23 @@
 import socket, time
 
-host = '127.0.0.1'
-port = 10000
+class Client : 
+    def __init__(self, host: str = '127.0.0.1', port: int = 10000):
+        self.host = host
+        self.port = port
+        self.client_socket = socket.socket()
 
-client_socket = socket.socket()
-client_socket.connect((host, port))
-print("Connecté au serveur.")
-    
-while True:
-    message = input("Client: ")
-    client_socket.send(message.encode())
-    reply = client_socket.recv(1024).decode()
-    print(f"Serveur: {reply}")
-        
-    if message.lower() in ["bye", "arret"]:
-        print("Déconnexion...")
-        client_socket.close()
-        break
+    def connexionserv(host,port):
+        print("Connecté au serveur."  )
+
+
+
+    while True:
+        message = input("Client: ")
+        client_socket.send(message.encode())
+        reply = client_socket.recv(1024).decode()
+        print(f"Serveur: {reply}")
+            
+        if message.lower() in ["bye", "arret"]:
+            print("Déconnexion...")
+            client_socket.close()
+            break
