11d447142dd45347d13b8bb21175d8088d3e4860
Mat6806 <147594936+Mat6806@users.noreply.github.com>
2024-11-20 15:00:43 +0000

    Vavantlesautresfonctions

--- Client.py
+++ Client.py
@@ -1,32 +1,34 @@
 import socket, time
 
 class Client : 
-    def __init__(self, host: str = '127.0.0.1', port: int = 10000):
+    def __init__(self, host: str = '127.0.0.1', port: int = 5000):
         self.host = host
         self.port = port
         self.client_socket = socket.socket()
 
     def connexionserv(self):
-        print("Connecté au serveur.")
+        
         try:
-            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client_socket.connect((self.host,self.port))
+            print("Connecté au serveur.")
+            while True:
+                message = input("Client: ")
+                self.client_socket.send(message.encode())   
+                reply = self.client_socket.recv(1024).decode()
+                print(f"Serveur: {reply}")
+                if message.lower() in ["bye", "arret"]:
+                    print("Déconnexion demandée...")
+                    break
         except ConnectionRefusedError as error:
             print(f"Connexion refusée : {error}")
         except TimeoutError as error:
             print(f"Délai dépassé : {error}")
-        
 
-            
+        print("Connecté au serveur.")
 
-'''
-    while True:
-        message = input("Client: ")
-        client_socket.send(message.encode())
-        reply = client_socket.recv(1024).decode()
-        print(f"Serveur: {reply}")
+
+if __name__ == "__main__":
+    # Création et démarrage du client
+    Connect = Client('127.0.0.1', 10000)
+    Connect.connexionserv()
             
-        if message.lower() in ["bye", "arret"]:
-            print("Déconnexion...")
-            client_socket.close()
-            '''
