013b55106443e62ce392adeb05652bac0fba72d0
Mat6806 <147594936+Mat6806@users.noreply.github.com>
2024-11-20 15:30:39 +0000

    v2

--- Client.py
+++ Client.py
@@ -7,28 +7,56 @@ class Client :
         self.client_socket = socket.socket()
 
     def connexionserv(self):
-        
         try:
-            self.client_socket.connect((self.host,self.port))
-            print("Connecté au serveur.")
+            self.client_socket.connect((self.host, self.port))
+            print(f"Connecté au serveur {self.host}:{self.port}")
+        except ConnectionRefusedError as erorr:
+            print(f"Connexion refusée : {erorr}")
+        except TimeoutError as erorr:
+            print(f"Délai dépassé : {erorr}")
+    
+    def envoimessage(self, message : str):
+        try:
+            self.client_socket.send(message.encode())
+            reply = self.client_socket.recv(1024).decode()
+            return reply
+        except Exception as erorr:
+            print(f"impossible d'envoyer le msg : {erorr}")
+    
+    '''test'''
+    
+    
+    
+    def close_connection(self):
+        """
+        Ferme la connexion au serveur.
+        """
+        if self.client_socket:
+            self.client_socket.close()
+            print("Connexion fermée.")
+
+    def start_interaction(self):
+        """
+        Démarre l'interaction entre le client et le serveur.
+        """
+        self.connect()
+
+        try:
             while True:
                 message = input("Client: ")
-                self.client_socket.send(message.encode())   
-                reply = self.client_socket.recv(1024).decode()
-                print(f"Serveur: {reply}")
                 if message.lower() in ["bye", "arret"]:
                     print("Déconnexion demandée...")
+                    self.send_message(message)
                     break
-        except ConnectionRefusedError as error:
-            print(f"Connexion refusée : {error}")
-        except TimeoutError as error:
-            print(f"Délai dépassé : {error}")
-
-        print("Connecté au serveur.")
+                reply = self.send_message(message)
+                print(f"Serveur: {reply}")
+        finally:
+            self.close_connection()
 
 
 if __name__ == "__main__":
     # Création et démarrage du client
-    Connect = Client('127.0.0.1', 10000)
-    Connect.connexionserv()
+    clien1 = Client('127.0.0.1', 5000)
+    clien1.connexionserv()
+    clien1.start_interaction
             
